63a9cb0dcb9b5683e39293a930d57c56b260d83f
KattyTotoro <89184036854@yandex.ru>
2024-12-09 00:46:49 +0300

    dfgdg

---
+++ stores/posts.ts
@@ -0,0 +1,13 @@
+// import { defineStore } from 'pinia'
+// import { ref } from 'vue'
+
+// export const usePosts = defineStore('posts', () => {
+//   const authors = [
+//     { id: 1, name: 'DFHjklklkkl' }
+//   ]
+  
+//   const posts = [
+//     { id: 1, title: 'First post', text: 'sdf sf hdkjhfsdhfjkh ofhdsjlkfn', preview: 'sdf sf hdkjhfsdhfjkh...', img: 'i.webp', datetime:'2024-11-28', author_id: 1 }
+//   ]
+//   return { posts, authors }
+// })
