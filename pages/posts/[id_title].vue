216d6f0e285365e3e0634fdb39bc6305f19b84cc
KattyTotoro <89184036854@yandex.ru>
2024-12-12 02:28:14 +0300

    asdf

--- pages/posts/[id_title].vue
+++ pages/posts/[id_title].vue
@@ -1,16 +1,26 @@
 <template>
-  <section class="content">
+  <section class="content" style="display: flex;">
   
-  <div class="postPage">
-  {{ route.params.id_title }}
-  <br>
-  {{ id }}
-  <br>
-  {{ post?.title }}
-  <br>
-  <NuxtImg :src="`img/${post?.img}`" sizes="300px"/>
-      
-  <div v-html="post?.text"></div>
+  <div v-if="post" class="postPage">
+    {{ route.params.id_title }}
+    <br>
+    {{ id }}
+    <br>
+    <h1>{{ post?.title }}</h1>
+    <br>
+    <NuxtImg :src="`img/${post?.img}`" sizes="300px"/>
+        
+    <div v-html="post?.text"></div>
+  </div>
+  <div v-else>Что-то пошло не так...</div>
+
+  <p v-if="!postsStore.posts.length">Публикаций пока нет</p>
+  <div v-else class="blockList">
+    <div class="post" v-for="post of postsStore.posts" :key="post.id">
+      <NuxtImg :src="`img/${post.img}`" sizes="300px"/>
+      <h2><NuxtLink :to="`/posts/${post.id}_${post.title.toLowerCase().replaceAll(' ','_')}`">{{ post.title }}</NuxtLink></h2>
+      <p>{{ post.preview }}</p>
+    </div>
   </div>
   </section>
 </template>
--- stores/posts.ts
+++ stores/posts.ts
@@ -3,12 +3,25 @@ import { ref } from 'vue'
 
 export const usePosts = defineStore('posts', () => {
   const posts = [
-    {id: 3, rubric: '', title: 'Third post', text: 'ВДОХНОВЕНИЕ - это Видеть, слышать, чувствовать…. Восхвалять творение, быть его частью ежесекундно! Приглашаем вместе учиться ведать Божественные энергии, делиться Радостью и Знаниями.', preview: 'third Peview ВДОХНОВЕНИЕ - это Видеть, слышать, чувствовать….', img: 'inspirationLargeImage.png',},
-    {id: 2, rubric: '', title: 'Second post', text: 'Second Text text text text', preview: 'SecondPeview...', img: 'summary_small_image.png', reactions:{
+    {id: 3, rubric: '', title: 'Third post', text: 'Text 1 ВДОХНОВЕНИЕ - это Видеть, слышать, чувствовать…. Восхвалять творение, быть его частью ежесекундно! Приглашаем вместе учиться ведать Божественные энергии, делиться Радостью и Знаниями.', preview: 'third Peview ВДОХНОВЕНИЕ - это Видеть, слышать, чувствовать….', 
+      img: 'inspirationLargeImage.png', datetime: '2024-12-12', author: 'Sidorov',
+      reactions:{
+        smile:0,
+        sad:0,
+      }},
+
+    {id: 2, rubric: '', title: 'Second post', text: 'Text 2 Second Text text text text ВДОХНОВЕНИЕ - это Видеть, слышать, чувствовать…. Восхвалять творение, быть его частью ежесекундно! Приглашаем вместе учиться ведать Божественные энергии, делиться Радостью и Знаниями.', 
+      preview: 'SecondPeview...', img: 'summary_small_image.png', datetime: '2024-12-12', author: 'Ivanov', 
+      reactions:{
       smile:0,
       sad:0,
     }},
-    {id: 1, rubric: '', title: 'First post', text: 'Text text text text', preview: 'Peview...', img: 'summary_large_image.png',},
+
+    {id: 1, rubric: '', title: 'First post', text: 'Text 3 Text text text text ВДОХНОВЕНИЕ - это Видеть, слышать, чувствовать…. Восхвалять творение, быть его частью ежесекундно! Приглашаем вместе учиться ведать Божественные энергии, делиться Радостью и Знаниями.', preview: 'Peview...', img: 'summary_large_image.png', datetime: '2024-12-12', author: 'Petrov',
+      reactions:{
+        smile:0,
+        sad:0,
+      }},
   ]
 
   return { posts }
